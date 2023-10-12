5cc7a17f094123aa63c9de8d9502d5bb5fca5ae5
Nolvos <119397000+Nolvos@users.noreply.github.com>
2023-10-12 20:33:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+async function checkAccuracy() {
+  const spokenText = document.getElementById('spokenText').innerText.split(':')[1].trim();
+
+  const response = await fetch('/api/check_accuracy', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ spokenText })
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    document.getElementById('accuracyResult').innerText = `Accuracy Result: ${result.accuracy}`;
+  } else {
+    console.error('Failed to check accuracy:', response.statusText);
+  }
+}
+
 const stories = [
   {
     title: 'The Lion and the Rabbit',
-    content: `Once there was a Lion in the jungle`,
+    content: `Once there was a Lion in the jungle...`,
   },
   {
     title: 'The Hunter and the Pigeons',
-    content: `One day a hunter sets a net to catch birds`,
+    content: `One day a hunter sets a net to catch birds...`,
   },
   // Add more stories in a similar format
 ];
@@ -43,22 +62,3 @@ recognition.onresult = (event) => {
 recognition.onerror = (event) => {
   console.error('Speech recognition error:', event.error);
 };
-async function checkAccuracy() {
-  const spokenText = document.getElementById('spokenText').innerText.split(':')[1].trim();
-
-  // Send a POST request to the server to calculate accuracy
-  const response = await fetch('/api/check_accuracy', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ spokenText })
-  });
-
-  if (response.ok) {
-    const result = await response.json();
-    document.getElementById('accuracyResult').innerText = `Accuracy Result: ${result}`;
-  } else {
-    console.error('Failed to check accuracy:', response.statusText);
-  }
-}
